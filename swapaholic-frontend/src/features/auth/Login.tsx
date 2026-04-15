504bdbfa04b44396710306cddac89eb9ed1bc169
Mehedi Hasan <mhshakib96@gmail.com>
2026-04-15 18:29:23 +0600

    remove google or facebook login functionality for demo purpose

--- swapaholic-frontend/src/features/auth/Login.tsx
+++ swapaholic-frontend/src/features/auth/Login.tsx
@@ -315,28 +315,6 @@ export const Login = () => {
                     </div>
                 </form>
 
-                <div className="mt-6">
-                    <div className="relative">
-                        <div className="absolute inset-0 flex items-center">
-                            <div className="w-full border-t border-gray-300 dark:border-slate-800" />
-                        </div>
-                        <div className="relative flex justify-center text-sm">
-                            <span className="px-2 bg-gray-50 dark:bg-slate-950 text-gray-500 dark:text-gray-400">
-                                Or continue with
-                            </span>
-                        </div>
-
-                    </div>
-
-                    <div className="mt-6 grid grid-cols-2 gap-3">
-                        <Button variant="outline" fullWidth onClick={() => window.location.href = resolveApiPath('/auth/google')}>
-                            Google
-                        </Button>
-                        <Button variant="outline" fullWidth onClick={() => window.location.href = resolveApiPath('/auth/facebook')}>
-                            Facebook
-                        </Button>
-                    </div>
-                </div>
             </div>
         </div>
     );
